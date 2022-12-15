c02f8889752f0c804adf8b43fc1303e66e3b8926
sadrakhsw <117900802+sadrakhsw@users.noreply.github.com>
2022-12-15 16:27:46 +0700

    Add files via upload

--- UG13_D_71220939.py
+++ UG13_D_71220939.py
@@ -2,6 +2,9 @@ import turtle
 
 t = turtle.Turtle()
 
+t.screen.bgcolor("blue")
+
+t.pencolor("white")
 t.right(35)
 t.pensize(20)
 t.up()
@@ -13,15 +16,26 @@ t.circle(40,220)
 t.up()
 t.goto(200,-40)
 t.pendown()
-t.left(125)
-t.circle(40,110)
+t.left(115)
+t.circle(40,120)
 t.forward(90)
 t.circle(30)
+t.up()
+t.goto(-240,-40)
+t.pendown()
+t.pendown()
+t.right(120)
+t.circle(40,120)
+t.forward(90)
+t.circle(30)
+t.up()
+t.goto(0,100)
 
 
 
 
-t.screen.bgcolor("purple")
+
+
 
 
 
